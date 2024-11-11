8456317cb33ef435dc840db143b673e6e854ae5f
sameer shaik <109144702+sameer15-hue@users.noreply.github.com>
2024-11-11 13:36:44 +0530

    promises

---
+++ deleted_files/promise/Boje.js
@@ -0,0 +1,17 @@
+function setTimeoutPromisified(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function solve() {
+	await setTimeoutPromisified(1000);
+	console.log("hi");
+	await setTimeoutPromisified(3000);
+	console.log("hello");
+	await setTimeoutPromisified(5000);
+	console.log("hi there");
+	console.log('sam');
+}
+
+solve();
+//basically we cant make setTimeout synchronous ,we just write await part as assuming that seTimeout in promise is sync .
+// but its also async only.
