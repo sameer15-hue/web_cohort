d3a022841263010372280282e06fe7c0663dc1c5
sameer shaik <109144702+sameer15-hue@users.noreply.github.com>
2024-11-25 13:15:47 +0530

    share link

--- week15/src/index.ts
+++ week15/src/index.ts
@@ -2,9 +2,8 @@
 import 'dotenv/config';
 import express from 'express';
 import jwt from 'jsonwebtoken';
-import { usermodel } from './db';
+import { usermodel,contentmodel, linkmodel } from './db';
 import authuser from './middlewear';
-const secretkey='ilovekiara'
 const app=express();
 app.use(express.json());
 app.get('/api/v1/signup',async (req,res)=>{
@@ -31,7 +30,7 @@ app.get('/api/v1/signin',async (req,res)=>{
     if(founduser){
         const token =jwt.sign({
             id:founduser._id.toString(),
-        },secretkey);
+        },process.env.secretkey as string);
         res.json({msg:token})
     }else{
         res.status(403).json({
@@ -41,20 +40,58 @@ app.get('/api/v1/signin',async (req,res)=>{
 })
 app.use(authuser);
 app.get('/api/v1/content',async (req,res)=>{
+    // @ts-ignore
+    const userid=req.id;
+    console.log(userid);
+    const response=await contentmodel.findOne({
+        user:userid,
+    }).populate("user","username");
+    res.json({
+        constent:response,
+    })
+})
+app.post("/api/v1/content", async(req, res) => {
     const {link,title}=req.body;
     // @ts-ignore
     const userid=req.id;
-    
-})
-app.post("/api/v1/content", (req, res) => {
+    await contentmodel.create({
+        link:link,
+        title:title,
+        user:userid,
+        tags:[]
+    })
+    res.json({msg:"content created!!"})
 
 });
-app.delete("/api/v1/content", (req, res) => {
-
+app.delete("/api/v1/content",async(req, res) => {
+    const contentid=req.body.contentid;
+    await contentmodel.deleteMany({
+        _id:contentid,
+        // @ts-ignore
+        user:req.id,
+    })
+    res.json({ msg: `content \`${contentid}\` deleted` });
 });
-
-app.post("/api/v1/brain/share", (req, res) => {
-
+app.post("/api/v1/brain/share", async(req, res) => {
+    const share=req.body.share;
+    const existinglink=linkmodel.findOne({
+        //@ts-ignore
+        user:req.id,
+    });
+    if(existinglink){
+        //@ts-ignore
+        const hash=existinglink.hash;
+        res.json({
+            msg:"link already exists!! ",
+            link:hash
+        })
+    }else{
+        const link=await linkmodel.create({
+            //@ts-ignore
+            user:req.id,
+            hash:Math.floor(Math.random() * 9000000000) + 1000000000
+        }) 
+   }
 });
 
 app.get("/api/v1/brain/:shareLink", (req, res) => {
--- week15/src/middlewear.ts
+++ week15/src/middlewear.ts
@@ -1,12 +1,11 @@
 import { NextFunction,Request,Response } from 'express';
 import jwt from 'jsonwebtoken';
 function authuser(req:Request,res:Response,next:NextFunction){
-    const token = req.headers["authorization"];
-    const response=jwt.verify(token!,process.env.secretkey!);
+    const token = req.headers.authorization;
+    const response=jwt.verify(token as string,process.env.secretkey as string);
     if(response){
-        console.log('next');
         // @ts-ignore
-        req.id=response.id
+        req.id=response.id;
         next();
     }else{
         res.status(401).json({message:'invalid token'})
